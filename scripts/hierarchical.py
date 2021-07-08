f8a0be2964a3c5c1b788168566cbf7a7147756e3
awj <alex.w.jung@googlemail.com>
2021-07-08 13:05:57 +0100

    missing-refined

--- scripts/hierarchical.py
+++ scripts/hierarchical.py
@@ -1,6 +1,6 @@
-# Modules
-# =======================================================================================================================
 
+# Module
+# =======================================================================================================================
 import tqdm
 import torch
 import numpy as np
@@ -12,7 +12,6 @@ from torch.distributions import constraints
 
 import pyro
 import pyro.distributions as dist
-from pyro.infer import Predictive
 
 import warnings
 warnings.filterwarnings("ignore")
@@ -21,21 +20,22 @@ import probcox as pcox
 
 dtype = torch.FloatTensor
 
-np.random.seed(7945)
-torch.manual_seed(1259)
-
+np.random.seed(34)
+torch.manual_seed(234)
 
 # Simulation Settings
 # =======================================================================================================================
-I = 1000 # Number of individuals
+
+# Male
+I = 250
 P_binary = 3
 P_continuous = 3
 P = P_binary + P_continuous
-theta = np.asarray([-0.5, 0.8, 1.4, 0.4, -0.9, 1])[:, None]
-scale = 3 # Scaling factor for Baseline Hazard
+theta_m = np.asarray([-0.7, 0.8, 0.6, 0, 0.9, -0.4])[:, None]
+scale = 6 # Scaling factor for Baseline Hazard
 
 # Class for simulation
-TVC = pcox.TVC(theta=theta, P_binary=P_binary, P_continuous=P_continuous, dtype=dtype)
+TVC = pcox.TVC(theta=theta_m, P_binary=P_binary, P_continuous=P_continuous, dtype=dtype)
 
 # Sample baseline hazard - scale is set to define censorship/events
 TVC.make_lambda0(scale=scale)
@@ -43,94 +43,57 @@ TVC.make_lambda0(scale=scale)
 # Sample Data
 np.random.seed(5)
 torch.manual_seed(4)
-surv = torch.zeros((0, 3))
-X = torch.zeros((0, P))
-Z = torch.zeros((0, I))
+surv_m = torch.zeros((0, 3))
+X_m = torch.zeros((0, 7))
 ii = 0
 for __ in (range(I)):
     a, b = TVC.sample(frailty=None)
-    Z_ = torch.zeros((a.shape[0], I))
-    Z_[:, ii] = 1
-    surv = torch.cat((surv, a))
-    X = torch.cat((X, b))
-    Z = torch.cat((Z, Z_))
+    b = torch.cat((b, torch.ones((b.shape[0], 1))), axis=1)
+    surv_m = torch.cat((surv_m, a))
+    X_m = torch.cat((X_m, b))
     ii += 1
 
+torch.sum(surv_m[:, -1]==1)
+plt.hist(surv_m[surv_m[:, -1]==1, 1].detach().numpy())
 
-# No missing data
+
+# Female
+I = 250
+P_binary = 3
+P_continuous = 3
+P = P_binary + P_continuous
+theta_m = np.asarray([-0.4, 0.9, 0, 0.5, 0.6, 0.2])[:, None]
+scale = 9 # Scaling factor for Baseline Hazard
+
+# Class for simulation
+TVC = pcox.TVC(theta=theta_m, P_binary=P_binary, P_continuous=P_continuous, dtype=dtype)
+
+# Sample baseline hazard - scale is set to define censorship/events
+TVC.make_lambda0(scale=scale)
+
+# Sample Data
+np.random.seed(9)
+torch.manual_seed(42)
+surv_f = torch.zeros((0, 3))
+X_f = torch.zeros((0, 7))
+ii = 0
+for __ in (range(I)):
+    a, b = TVC.sample(frailty=None)
+    b = torch.cat((b, torch.ones((b.shape[0], 1))), axis=1)
+    surv_f = torch.cat((surv_f, a))
+    X_f = torch.cat((X_f, b))
+    ii += 1
+torch.sum(surv_f[:, -1]==1)
+plt.hist(surv_f[surv_f[:, -1]==1, 1].detach().numpy())
+
+
+# Inference Male only
 # =======================================================================================================================
 def predictor(data):
-    theta =  pyro.sample("theta", dist.StudentT(1, loc=0, scale=0.001).expand([1, data[1].shape[1]])).type(dtype)
-    pred = torch.mm(data[1], theta.T)
+    theta =  pyro.sample("theta", dist.StudentT(1, loc=0, scale=0.001).expand([data[1].shape[1], 1])).type(dtype)
+    pred = torch.mm(data[1], theta)
     return(pred)
 
-def guide(data, rank=6):
-    cov_diag = pyro.param("cov_diag", torch.full((data[1].shape[1],), 0.001), constraint=constraints.positive)
-    cov_factor = pyro.param("cov_factor", torch.randn((data[1].shape[1], rank)) * 0.001)
-    loc = pyro.param('loc', torch.zeros(data[1].shape[1]))
-    pyro.sample("theta", dist.LowRankMultivariateNormal(loc, cov_factor, cov_diag).expand((1,)))
-
-def evaluate(surv, X, batchsize, sampling_proportion, iter_, predictor=predictor, guide=guide):
-    sampling_proportion[1] = batchsize
-    eta=1 # paramter for optimization
-    run = True # repeat initalization if NAN encounterd while training - gauge correct optimization settings
-    while run:
-        run = False
-        pyro.clear_param_store()
-        m = pcox.PCox(sampling_proportion=sampling_proportion, predictor=predictor, guide=guide)
-        m.initialize(eta=eta, num_particles=4)
-        loss=[0]
-        for ii in tqdm.tqdm(range((iter_))):
-            idx = np.unique(np.concatenate((np.random.choice(np.where(surv[:, -1]==1)[0], 1, replace=False), np.random.choice(range(surv.shape[0]), batchsize-1, replace=False)))) # random sample of data - force at least two events (no evaluation otherwise)
-            data=[surv[idx], X[idx]] # subsampled data
-            loss.append(m.infer(data=data))
-            # divergence check
-            if loss[-1] != loss[-1]:
-                eta = eta * 0.1
-                run=True
-                break
-
-    g = m.return_guide()
-    mm = m.return_model()
-    #out = g.quantiles([0.025, 0.5, 0.975])
-    plt.plot(loss)
-    return(g, mm)
-
-total_obs = surv.shape[0]
-total_events = torch.sum(surv[:, -1] == 1).numpy().tolist()
-pyro.clear_param_store()
-g, mm = evaluate(batchsize=512, iter_=50000, surv=surv, X=X, sampling_proportion=[total_obs, None, total_events, None])
-
-predictive = Predictive(model=mm, guide=g, num_samples=1000, return_sites=('theta', 'obs'))
-samples = predictive([surv, X])['theta']
-
-out = np.percentile(np.squeeze(samples.detach().numpy()), [5, 50, 95], axis=0)
-theta_est_lower = out[0, :][:, None]
-theta_est = out[1, :][:, None]
-theta_est_upper = out[2, :][:, None]
-
-fig, ax = plt.subplots(1, 1, figsize=(5, 5), dpi=100)
-ax.errorbar(theta[:, 0], theta_est[:, 0], yerr=(theta_est[:, 0] - theta_est_lower[:, 0], theta_est_upper[:, 0]- theta_est[:, 0]),  ls='', c=".3", capsize=2, capthick=0.95, elinewidth=0.95)
-ax.plot(theta[:, 0], theta_est[:, 0], ls='', c=".3", marker='x', ms=2)
-ax.set(xlim=(-2, 2), ylim=(-2, 2))
-ax.plot(ax.get_xlim(), ax.get_ylim(), ls="--", c="red", linewidth=0.75)
-ax.set_yticks([-2, 0, 2])
-ax.set_ylim([-2, 2])
-ax.set_xticks([-2, 0, 2])
-ax.set_xlim([-2, 2])
-ax.set_xlabel('theta')
-ax.set_ylabel('theta_hat')
-plt.show()
-plt.close()
-
-# Set missingness
-# =======================================================================================================================
-idx_missing = torch.rand(X.shape) < 0.25
-X[idx_missing] = float('nan')
-
-
-# Complete case
-# =======================================================================================================================
 def evaluate(surv, X, batchsize, sampling_proportion, iter_, predictor=predictor):
     sampling_proportion[1] = batchsize
     eta=5 # paramter for optimization
@@ -139,10 +102,10 @@ def evaluate(surv, X, batchsize, sampling_proportion, iter_, predictor=predictor
         run = False
         pyro.clear_param_store()
         m = pcox.PCox(sampling_proportion=sampling_proportion, predictor=predictor)
-        m.initialize(eta=eta, num_particles=1, rank=6)
+        m.initialize(eta=eta, num_particles=1, rank=7)
         loss=[0]
         for ii in tqdm.tqdm(range((iter_))):
-            idx = np.unique(np.concatenate((np.random.choice(np.where(surv[:, -1]==1)[0], 1, replace=False), np.random.choice(range(surv.shape[0]), batchsize, replace=False))))[:batchsize] # random sample of data - force at least two events (no evaluation otherwise)
+            idx = np.unique(np.concatenate((np.random.choice(np.where(surv[:, -1]==1)[0], 2, replace=False), np.random.choice(range(surv.shape[0]), batchsize-2, replace=False)))) # random sample of data - force at least two events (no evaluation otherwise)
             data=[surv[idx], X[idx]] # subsampled data
             loss.append(m.infer(data=data))
             # divergence check
@@ -154,19 +117,19 @@ def evaluate(surv, X, batchsize, sampling_proportion, iter_, predictor=predictor
     out = g.quantiles([0.025, 0.5, 0.975])
     return(out)
 
-total_obs = surv[torch.sum(idx_missing, axis=1) == 0].shape[0]
-total_events = torch.sum(surv[torch.sum(idx_missing, axis=1) == 0][:, -1] == 1).numpy().tolist()
+total_obs = surv_m.shape[0]
+total_events = torch.sum(surv_m[:, -1] == 1).numpy().tolist()
 pyro.clear_param_store()
-out = evaluate(batchsize=512, iter_=50000, surv=surv[torch.sum(idx_missing, axis=1) == 0], X=X[torch.sum(idx_missing, axis=1) == 0], sampling_proportion=[total_obs, None, total_events, None])
+out = evaluate(batchsize=512, iter_=25000, surv=surv_m, X=X_m, sampling_proportion=[total_obs, None, total_events, None])
 
 # plot the results
-theta_est = out['theta'][1].detach().numpy().T
-theta_est_lower = out['theta'][0].detach().numpy().T
-theta_est_upper = out['theta'][2].detach().numpy().T
+theta_est = out['theta'][1].detach().numpy()
+theta_est_lower = out['theta'][0].detach().numpy()
+theta_est_upper = out['theta'][2].detach().numpy()
 
 fig, ax = plt.subplots(1, 1, figsize=(5, 5), dpi=100)
-ax.errorbar(theta[:, 0], theta_est[:, 0], yerr=(theta_est[:, 0] - theta_est_lower[:, 0], theta_est_upper[:, 0]- theta_est[:, 0]),  ls='', c=".3", capsize=2, capthick=0.95, elinewidth=0.95)
-ax.plot(theta[:, 0], theta_est[:, 0], ls='', c=".3", marker='x', ms=2)
+ax.errorbar(theta_m[:6, 0], theta_est[:6, 0], yerr=(theta_est[:6, 0] - theta_est_lower[:6, 0], theta_est_upper[:6, 0]- theta_est[:6, 0]),  ls='', c=".3", capsize=2, capthick=0.95, elinewidth=0.95)
+ax.plot(theta_m[:6, 0], theta_est[:6, 0], ls='', c=".3", marker='x', ms=2)
 ax.set(xlim=(-2, 2), ylim=(-2, 2))
 ax.plot(ax.get_xlim(), ax.get_ylim(), ls="--", c="red", linewidth=0.75)
 ax.set_yticks([-2, 0, 2])
@@ -176,38 +139,77 @@ ax.set_xlim([-2, 2])
 ax.set_xlabel('theta')
 ax.set_ylabel('theta_hat')
 plt.show()
+plt.close()
 
 
-# Imputed
+
+
+# Inference Female only
+# =======================================================================================================================
+def predictor(data):
+    theta =  pyro.sample("theta", dist.StudentT(1, loc=0, scale=0.001).expand([data[1].shape[1], 1])).type(dtype)
+    pred = torch.mm(data[1], theta)
+    return(pred)
+
+def evaluate(surv, X, batchsize, sampling_proportion, iter_, predictor=predictor):
+    sampling_proportion[1] = batchsize
+    eta=5 # paramter for optimization
+    run = True # repeat initalization if NAN encounterd while training - gauge correct optimization settings
+    while run:
+        run = False
+        pyro.clear_param_store()
+        m = pcox.PCox(sampling_proportion=sampling_proportion, predictor=predictor)
+        m.initialize(eta=eta, num_particles=1, rank=7)
+        loss=[0]
+        for ii in tqdm.tqdm(range((iter_))):
+            idx = np.unique(np.concatenate((np.random.choice(np.where(surv[:, -1]==1)[0], 2, replace=False), np.random.choice(range(surv.shape[0]), batchsize-2, replace=False)))) # random sample of data - force at least two events (no evaluation otherwise)
+            data=[surv[idx], X[idx]] # subsampled data
+            loss.append(m.infer(data=data))
+            # divergence check
+            if loss[-1] != loss[-1]:
+                eta = eta * 0.1
+                run=True
+                break
+    g = m.return_guide()
+    out = g.quantiles([0.025, 0.5, 0.975])
+    return(out)
+
+total_obs = surv_f.shape[0]
+total_events = torch.sum(surv_f[:, -1] == 1).numpy().tolist()
+pyro.clear_param_store()
+out = evaluate(batchsize=512, iter_=25000, surv=surv_f, X=X_f, sampling_proportion=[total_obs, None, total_events, None])
+
+# plot the results
+theta_est = out['theta'][1].detach().numpy()
+theta_est_lower = out['theta'][0].detach().numpy()
+theta_est_upper = out['theta'][2].detach().numpy()
+
+fig, ax = plt.subplots(1, 1, figsize=(5, 5), dpi=100)
+ax.errorbar(theta_f[:6, 0], theta_est[:6, 0], yerr=(theta_est[:6, 0] - theta_est_lower[:6, 0], theta_est_upper[:6, 0]- theta_est[:6, 0]),  ls='', c=".3", capsize=2, capthick=0.95, elinewidth=0.95)
+ax.plot(theta_f[:6, 0], theta_est[:6, 0], ls='', c=".3", marker='x', ms=2)
+ax.set(xlim=(-2, 2), ylim=(-2, 2))
+ax.plot(ax.get_xlim(), ax.get_ylim(), ls="--", c="red", linewidth=0.75)
+ax.set_yticks([-2, 0, 2])
+ax.set_ylim([-2, 2])
+ax.set_xticks([-2, 0, 2])
+ax.set_xlim([-2, 2])
+ax.set_xlabel('theta')
+ax.set_ylabel('theta_hat')
+plt.show()
+plt.close()
+
+
+
+
+# Inference Joint
 # =======================================================================================================================
 
+
+
+
+
 def predictor(data, dtype=dtype):
-    # imputation
-    quant_mu = pyro.sample("quant_mu", dist.Normal(0, 1).expand([3])).type(dtype)
-    quant_sigma = pyro.sample("quant_sigma", dist.Uniform(0.5, 1.5).expand([3])).type(dtype)
-    quant_impute = pyro.sample("quant_impute", dist.Normal(quant_mu, quant_sigma).expand([512, 3]).mask(False)).type(dtype)
 
-    qual_p = pyro.sample("qual_p", dist.Beta(1, 1).expand([3])).type(dtype)
-    qual_impute = pyro.sample("qual_impute", dist.Uniform(0, 1).expand([512, 3])).type(dtype)
-    qual_impute = (qual_impute <= qual_p).type(dtype)
-
-    X_ = data[1].clone()
-    X_[:, :3][data[2][:, :3]] = qual_impute[data[2][:, :3]]
-    X_[:, 3:][data[2][:, 3:]] = quant_impute[data[2][:, 3:]]
-
-    #X_[:, 3:][data[2][:, 3:]] = qual_impute[data[2][:, 3:]]
-    pyro.sample("qual_unobserved1", dist.Bernoulli(qual_p[0]), obs=X_[:, 0][~data[2][:, 0]])
-    pyro.sample("qual_unobserved2", dist.Bernoulli(qual_p[1]), obs=X_[:, 1][~data[2][:, 1]])
-    pyro.sample("qual_unobserved3", dist.Bernoulli(qual_p[2]), obs=X_[:, 2][~data[2][:, 2]])
-
-    pyro.sample("quant_unobserved1", dist.Normal(quant_mu[0], quant_sigma[0]), obs=X_[:, 3][~data[2][:, 3]])
-    pyro.sample("quant_unobserved2", dist.Normal(quant_mu[1], quant_sigma[1]), obs=X_[:, 4][~data[2][:, 4]])
-    pyro.sample("quant_unobserved3", dist.Normal(quant_mu[2], quant_sigma[2]), obs=X_[:, 5][~data[2][:, 5]])
-
-    #pyro.sample("var2", dist.Bernoulli(qual_p), obs=X_[:, 3:])
-
-    # inference
-    #theta =  pyro.sample("theta", dist.Normal(loc=0, scale=1).expand([data[1].shape[1], 1])).type(dtype)
     theta =  pyro.sample("theta", dist.StudentT(1, loc=0, scale=0.001).expand([1, data[1].shape[1]])).type(dtype)
     pred = torch.mm(X_, theta.T)
     return(pred)
@@ -226,13 +228,12 @@ def guide(data, rank=6):
     quant_impute = pyro.sample("quant_impute", dist.Normal(quant_mu, quant_sigma).expand([512, 3]).mask(False)).type(dtype)
 
     qual_p = pyro.sample("qual_p", dist.Beta(qual_q1, qual_q2).expand([3])).type(dtype)
-    qual_impute = pyro.sample("qual_impute", dist.Uniform(0, 1).expand([512, 3]).mask(False)).type(dtype)
+    qual_impute = pyro.sample("qual_impute", dist.Uniform(0, 1).expand([512, 3])).type(dtype)
 
     cov_diag = pyro.param("cov_diag", torch.full((data[1].shape[1],), 0.01), constraint=constraints.positive)
     cov_factor = pyro.param("cov_factor", torch.randn((data[1].shape[1], rank)) * 0.01)
     loc = pyro.param('loc', torch.zeros(data[1].shape[1]))
     pyro.sample("theta", dist.LowRankMultivariateNormal(loc, cov_factor, cov_diag).expand((1,)))
-
 
 def evaluate(surv, X, batchsize, sampling_proportion, iter_, predictor=predictor, guide=guide):
     sampling_proportion[1] = batchsize
--- src/probcox/probcox.py
+++ src/probcox/probcox.py
@@ -56,9 +56,10 @@ class CoxPartialLikelihood(dist.TorchDistribution):
 # -----------------------------------------------------------------------------------------------------------------------------
 
 class PCox():
-    def __init__(self, predictor=None, guide=None, optimizer=None, loss=None, sampling_proportion=None, dtype=dtype):
+    def __init__(self, predictor=None, guide=None, model=None, optimizer=None, loss=None, sampling_proportion=None, dtype=dtype):
         self.predictor = predictor
         self.guide = guide
+        self.guide = model
         self.optimizer = optimizer
         self.loss = loss
         self.sampling_proportion = sampling_proportion
@@ -66,22 +67,25 @@ class PCox():
         super().__init__()
 
     def model(self, data):
-        # stoachstic update -likelihood adjustment
-        if self.sampling_proportion:
-            self.sampling_proportion[0] = torch.tensor([self.sampling_proportion[0]])
-            self.sampling_proportion[1] = torch.tensor([self.sampling_proportion[1]])
-            self.sampling_proportion[2] = torch.tensor([self.sampling_proportion[2]])
-            self.sampling_proportion[3] = torch.sum(data[0][:, -1])
-
-        # prior
-        if self.predictor:
-            pred = self.predictor(data)
+        if self.guide:
+            self.guide = self.guide
         else:
-            theta =  pyro.sample("theta", dist.Normal(0, 1).expand([data[1].shape[1], 1])).type(self.dtype)
-            pred = torch.mm(data[1], theta)
+            # stoachstic update -likelihood adjustment
+            if self.sampling_proportion:
+                self.sampling_proportion[0] = torch.tensor([self.sampling_proportion[0]])
+                self.sampling_proportion[1] = torch.tensor([self.sampling_proportion[1]])
+                self.sampling_proportion[2] = torch.tensor([self.sampling_proportion[2]])
+                self.sampling_proportion[3] = torch.sum(data[0][:, -1])
 
-        # Likelihood
-        pyro.sample("obs", CoxPartialLikelihood(pred=pred, sampling_proportion=self.sampling_proportion, dtype=self.dtype), obs=data[0])
+            # prior
+            if self.predictor:
+                pred = self.predictor(data)
+            else:
+                theta =  pyro.sample("theta", dist.Normal(0, 1).expand([data[1].shape[1], 1])).type(self.dtype)
+                pred = torch.mm(data[1], theta)
+
+            # Likelihood
+            pyro.sample("obs", CoxPartialLikelihood(pred=pred, sampling_proportion=self.sampling_proportion, dtype=self.dtype), obs=data[0])
 
     def make_guide(self, rank):
         if self.guide:
